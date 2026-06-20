64cd38e3f1d06d75abb1c2f246ec7aab04a75089
lopatin.maxim <lopatin.maxim2012@yandex.ru>
2026-06-20 16:43:56 +0300

    bugfix;Удалена тестовая альфа-ссылка на тест получения динамического токена

--- app_page/urls.py
+++ app_page/urls.py
@@ -4,5 +4,4 @@ from app_page import views
 
 urlpatterns = [
     path('', view=views.HomePageView.as_view(), name='home'),
-    path('test-true-api/', views.test_true_api_connection, name='test_true_api'),
 ]
